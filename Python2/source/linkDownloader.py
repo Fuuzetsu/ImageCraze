ff5c77fc833c6203e419ae5cbaf0ac884dd3fd7c
ShanaTsunTsunLove <darthmaulster@hotmail.com>
2011-09-22 06:09:32 +0100

    indent change

--- Python2/source/linkDownloader.py
+++ Python2/source/linkDownloader.py
@@ -10,29 +10,29 @@ def download(saveDestination, sourceLink):
 		os.chdir(saveDestination)
 		tried = 0
 		maxTries = 3
-            while maxTries > tried:
-                #Download the file
-                try:
-                    #Download the data and file size information
-                    fileData = urllib.urlopen(linkToFile)
-                    fileSize = int(fileData.headers['Content-Length'])
-                    fileData = fileData.read()
+        	while maxTries > tried:
+            	#Download the file
+				try:
+					#Download the data and file size information
+					fileData = urllib.urlopen(linkToFile)
+					fileSize = int(fileData.headers['Content-Length'])
+					fileData = fileData.read()
+
+					#Obtain file name
+					saveFileTo = os.path.split(linkToFile)[1]
+
+					#Save the file
+					fileSaveObject = open(saveFileTo, 'wb')
+					fileSaveObject.write(fileData)
+					fileSaveObject.close()
                     
-                    #Obtain file name
-                    saveFileTo = os.path.split(linkToFile)[1]
-                    
-                    #Save the file
-                    fileSaveObject = open(saveFileTo, 'wb')
-                    fileSaveObject.write(fileData)
-                    fileSaveObject.close()
-                    
-                    #Check that the file size matches
-                    if not os.path.getsize(saveFileTo) == fileSize:
-                        tried += 1
-                        continue
-                    else:
-                        break
-                except:
-                    tried += 1
-            #Go back to the initial directory
-            os.chdir(originalDirectory)
+					#Check that the file size matches
+					if not os.path.getsize(saveFileTo) == fileSize:
+						tried += 1
+						continue
+					else:
+						break
+				except:
+					tried += 1
+				#Go back to the initial directory
+				os.chdir(originalDirectory)
