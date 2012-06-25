66b6321ed954c4572f57cd0738c0849ed01a0a9f
Golevka <xu_michelle@live.com>
2012-06-25 16:53:30 +0800

    user interface refined: removed check button (check is done before downloading); get "~" properly expanded in path

--- ImageCraze.py
+++ ImageCraze.py
@@ -23,7 +23,7 @@ class App:
 		#Checkboxes
 		self.gelbooruCheck = Checkbutton(master, text = 'Gelbooru', variable = self.listOfSites['gelbooruCheck'], command = self.entryBoxState)
 		self.konachanCheck = Checkbutton(master, text = 'Konachan', variable = self.listOfSites['konachanCheck'], command = self.entryBoxState)
-		self.ichijouCheck = Checkbutton(master, text = 'Ichijou', variable = self.listOfSites['ichijouCheck'], command = self.entryBoxState)		
+		self.ichijouCheck = Checkbutton(master, text = 'Ichijou', variable = self.listOfSites['ichijouCheck'], command = self.entryBoxState)
 		self.danbooruCheck = Checkbutton(master, text = 'Danbooru', variable = self.listOfSites['danbooruCheck'], command = self.entryBoxState)
 		self.sankakuComplexCheck = Checkbutton(master, text = 'Sankaku', variable = self.listOfSites['sankakuComplexCheck'], command = self.entryBoxState)
 		self.safebooruCheck = Checkbutton(master, text = 'Safebooru', variable = self.listOfSites['safebooruCheck'], command = self.entryBoxState)
@@ -57,9 +57,6 @@ class App:
 		self.tagBox['width'] = 30
 		self.tagBox.grid(row = 5, column = 1, columnspan = 3, sticky = W+N)
 
-		#Check button
-		self.checkButton = Button(master, text = 'Check', state = DISABLED, disabledforeground = 'grey', command = self.checkSetup)
-		self.checkButton.grid(row = 6, column = 0)
 		
 		#Download button
 		self.downloadButton = Button(master, text = 'Download', state = DISABLED, command = self.startDownload)
@@ -90,7 +87,7 @@ class App:
 			self.tickAllVar = False
 			self.saveDestinationBox['state'] = NORMAL
 			self.tagBox['state'] = NORMAL
-			self.checkButton['state'] = NORMAL
+                        self.downloadButton['state'] = NORMAL
 		else:
 			self.gelbooruCheck.deselect()
 			self.konachanCheck.deselect()
@@ -104,54 +101,52 @@ class App:
 			self.tickAllVar = True		
 			self.saveDestinationBox['state'] = DISABLED
 			self.tagBox['state'] = DISABLED
-			self.checkButton['state'] = DISABLED
+                        self.downloadButton['state'] = DISABLED
 			
 
+        # Check if all settings were valid before downloading images
 	def checkSetup(self):
-		emptyTag = 0
-		emptyDestination = 0
-		wrongDestination = 0
-		notDestination = 0
+		emptyTag = 0              # tag is empty
+		emptyDestination = 0      # destination path is empty
+		wrongDestination = 0      # destination path is does not exist
+		notDestination = 0        # path specified does not lead to a  directory
+
+                # we need to get '~' properly expanded
+                save_dest = os.path.expanduser(self.saveDestinationBox.get())
+
+                # check for errors
 		if self.tagBox.get() == '':
 			emptyTag = 1
 
 		if self.saveDestinationBox.get() == '':
 			emptyDestination = 1
-		elif not os.path.exists(self.saveDestinationBox.get()):
+		elif not os.path.exists(save_dest):
 			wrongDestination = 1
-		elif not os.path.isdir(self.saveDestinationBox.get()):
+		elif not os.path.isdir(save_dest):
 			notDestination = 1
 
 		#Construct error message
 		error = ''
 		if emptyDestination: error += 'Nothing in destination box.\n '
 		if wrongDestination: error += 'Destination doesn\'t exist.\n '
-		if notDestination: error += 'Destination is not a directory.\n '
-		if emptyTag: error += 'Nothing in the tag box. '
+		if notDestination:   error += 'Destination is not a directory.\n '
+		if emptyTag:         error += 'Nothing in the tag box. '
 
 		if not error == '':
 			self.messageLabel['foreground'] = 'red'
 			self.messageLabel['text'] = '\n' + error
+                        return False
 		else:
 			self.messageLabel['foreground'] = 'green'
 			self.messageLabel['text'] = '\nAll checked out. Ready to go!\n'
 			self.downloadButton['state'] = NORMAL
-			self.cacheDestination = self.saveDestinationBox.get()
+			self.cacheDestination = save_dest
 			self.cacheSites = self.listOfSites.copy()
 			self.cacheTags = self.tagBox.get()
+                        return True
 			
-		#Check download sources
-		gotSource = False
-		for key in self.listOfSites.keys():
-			if self.listOfSites[key].get() == 1:
-				gotSource = True
-				break
+
 		
-		#Check save destination
-		if not os.path.exists(self.saveDestinationBox.get()):
-			print 'Destination %s doesn\'t exist.' % os.path.abspath(self.saveDestinationBox.get())
-		
-	
 	def entryBoxState(self):
 		#Check download sources and set saveDestinationBox and allButton states and variables
 		gotSource = 0
@@ -161,33 +156,28 @@ class App:
 
 		if gotSource == len(self.listOfSites.keys()):
 			self.saveDestinationBox['state'] = NORMAL
-			self.checkButton['state'] = NORMAL
+                        self.downloadButton['state'] = NORMAL
 			self.tagBox['state'] = NORMAL
 			self.allButton['text'] = 'None'
 			self.tickAllVar = False
 		elif gotSource > 0:
 			self.saveDestinationBox['state'] = NORMAL
-			self.checkButton['state'] = NORMAL
+                        self.downloadButton['state'] = NORMAL
 			self.tagBox['state'] = NORMAL
 			self.allButton['text'] = 'All'
 			self.tickAllVar = True
 		else:
 			self.saveDestinationBox['state'] = DISABLED
-			self.checkButton['state'] = DISABLED
+                        self.downloadButton['state'] = DISABLED
 			self.tagBox['state'] = DISABLED
 			self.allButton['text'] = 'All'
 			self.tickAllVar = True
 
 	
 	def startDownload(self):
-		#Make sure user didn't change anything
-		if not self.cacheSites == self.listOfSites or not self.cacheTags == self.tagBox.get() or not self.cacheDestination == self.saveDestinationBox.get():
-			self.messageLabel['foreground'] = 'red'
-			self.messageLabel['text'] = '\nDon\'t change the settings after checking.\nCheck again before downloading.'
-			self.downloadButton['state'] = DISABLED
-		else:
-			workerThread = threading.Thread(target = mainWorker.work, args = (self,))
-			workerThread.start()
+                if self.checkSetup():
+                        workerThread = threading.Thread(target = mainWorker.work, args = (self,))
+                        workerThread.start()
 
 
 	def enabler(self, enableRequest):
@@ -195,7 +185,6 @@ class App:
 			self.downloadButton['state'] = DISABLED
 			self.tagBox['state'] = DISABLED
 			self.saveDestinationBox['state'] = DISABLED
-			self.checkButton['state'] = DISABLED
 			self.allButton['state'] = DISABLED
 			self.gelbooruCheck['state'] = DISABLED
 			self.konachanCheck['state'] = DISABLED
@@ -209,7 +198,6 @@ class App:
 			self.downloadButton['state'] = NORMAL
 			self.tagBox['state'] = NORMAL
 			self.saveDestinationBox['state'] = NORMAL
-			self.checkButton['state'] = NORMAL
 			self.allButton['state'] = NORMAL
 			self.gelbooruCheck['state'] = NORMAL
 			self.konachanCheck['state'] = NORMAL
