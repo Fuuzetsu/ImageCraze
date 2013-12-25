b47e883e3c0f8b71a127e66cfa640905c95bfefa
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-25 14:33:43 +0000

    Fix whitespace

--- source/sourceParser.py
+++ source/sourceParser.py
@@ -2,30 +2,30 @@
 import xml.dom.minidom, json, os
 from lxml import etree
 from StringIO import *
-def parserXML(source):  
+def parserXML(source):
     mapping = {}
     for ev, el in etree.iterparse(StringIO(source)):
-	    if el.tag == 'post':
-		        mapping[el.attrib['md5']] = el.attrib['file_url']
+            if el.tag == 'post':
+                        mapping[el.attrib['md5']] = el.attrib['file_url']
     return mapping
 
 def parserJSON(jsonsource):
-	split = json.loads(jsonsource)
-	mapping = {}
+        split = json.loads(jsonsource)
+        mapping = {}
 
-	for dictionary in split:
-		link = dictionary['file_url']
-		md5 = dictionary['md5']
-		mapping[md5] = link
+        for dictionary in split:
+                link = dictionary['file_url']
+                md5 = dictionary['md5']
+                mapping[md5] = link
 
-	return mapping
-		
+        return mapping
+
 
 def parse(source):
-	imageDictionary = {}
-	if source[0] == '[':
-		imageDictionary = parserJSON(source)
-	elif source[0] == '<':
-		imageDictionary = parserXML(source)
+        imageDictionary = {}
+        if source[0] == '[':
+                imageDictionary = parserJSON(source)
+        elif source[0] == '<':
+                imageDictionary = parserXML(source)
 
-	return imageDictionary
+        return imageDictionary
